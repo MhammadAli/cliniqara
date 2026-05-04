f7aea048ddae4217639e30740fcdcc881f7c495f
MhammadAli <mohammed180726@feng.bu.edu.eg>
2026-05-04 23:24:45 +0300

    Add Failure class and PatientRepository interface for error handling and patient management

---
+++ lib/domain/repositories/patient_repository.dart
@@ -0,0 +1,11 @@
+import 'package:dartz/dartz.dart';
+import '../../core/error/failures.dart';
+import '../entities/patient.dart';
+
+abstract class PatientRepository {
+  Future<Either<Failure, Unit>> registerPatient(Patient patient);
+  Future<Either<Failure, Patient>> getPatientById(String id);
+  Future<Either<Failure, List<Patient>>> getAllPatients();
+  Future<Either<Failure, Unit>> updatePatient(Patient patient);
+  Future<Either<Failure, Unit>> deletePatient(String id);
+}
